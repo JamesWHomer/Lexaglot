a26833111debad95dbcfd7f93dbc07a4957d4142
JamesSurface <ubersuperrareboss@gmail.com>
2025-01-11 16:34:57 +1100

    ranom

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from auth import get_current_active_user
 from auth_models import User
 from typing import Dict, Any, Optional
 from datetime import datetime
+import random 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -81,7 +82,7 @@ async def next_exercise(
     language: str,
     current_user: User = Depends(get_current_active_user)
 ): 
-    return await database.get_exercise_by_id('676f6768de36cfc42c3e7dcf') # Only temporary, will replace with cached exercise later
+    return await database.get_exercise_by_id(random.choice('676f6768de36cfc42c3e7dcf', '676f721355bddf0ac8513ea6')) # Only temporary, will replace with cached exercise later
 
 # @app.put("/tokenbank/{language}")
 # async def update_tokenbank(
